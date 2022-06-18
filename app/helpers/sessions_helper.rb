4ccd6d52eb927e63b7a29510f721a23a84cb13df
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-06-18 14:51:24 -0300

    Show a user-specific header on all pages when logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
