9dc48d3d9acbf4efcbad4236bacadfaf2ee48b93
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-06-18 15:36:58 -0300

    Link to logout and delete user id from the session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "users#new"
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
